ec4845a98914ea8fc9f443f49b72b83dee73d4e7
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-11-07 06:45:40 -0700

    utils finalizing

--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,8 @@
-// lock routes to logged in users only
+// logged in users only have access
 function withAuth(req, res, next) {
     if (!req.session.user_id) {
         res.redirect('/login');
     } else {
-        // logged in, so send to next middleware
         next();
     }
 };
