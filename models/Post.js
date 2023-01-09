e4c024a45c61af3cd8c61f1bd82bbc2966fc1083
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-09 14:47:04 -0700

    refactor post.js

--- models/Post.js
+++ models/Post.js
@@ -1,66 +1,60 @@
-const req = require('express/lib/request');
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const connection = require('../config/connection');
 
-// post extends connetion model
 class Post extends Model {
-    static upvote(body, models) {
-        return models.Vote.create({
-            user_id: body.user_id,
-            post_id: body.post_id
-        })
-            .then(() => {
-                return Post.findOne({
-                    where: {
-                        id: body.post_id
-                    },
-                    attributes: [
-                        'id',
-                        'post_title', 
-                        'post_body', 
-                        'created_at',
-                        [sequelize.literal(`(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)`), 'vote_count']
-                    ]
-                });
-            });
-    }
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      post_id: body.post_id
+    })
+      .then(() => {
+        return Post.findOne({
+          where: {
+            id: body.post_id
+          },
+          attributes: [
+            'id',
+            'post_title',
+            'post_body',
+            'created_at',
+            [connection.literal(`(SELECT COUNT (*) FROM votes WHERE posts.id = votes.post_id)`), 'vote_count']
+          ]
+        });
+      });
+  }
 }
 
-Post.init(
-
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        post_title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [3]
-            }
-        },
-        post_body: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'post'
+Post.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  post_title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [3]
     }
-);
+  },
+  post_body: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'users',
+      key: 'id'
+    },
+    allowNull: false
+  }
+}, {
+  sequelize: connection,
+  modelName: 'posts',
+  underscored: true,
+  freezeTableName: true
+});
 
 module.exports = Post;
