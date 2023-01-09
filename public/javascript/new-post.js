e4689f9992f7c1f29b47661f4f69bae59539136c
Spencer Gerritsen <spencergerritsen@gmail.com>
2023-01-09 14:57:27 -0700

    refactor new-post.js

--- public/javascript/new-post.js
+++ public/javascript/new-post.js
@@ -1,28 +1,23 @@
-async function newPostFormHandler(e) {
+const submitNewPost = async (e) => {
     e.preventDefault();
-
-    const post_title = document.querySelector('#post-title').value.trim();
-    const post_body = document.querySelector('#post-body').value.trim();
-    
-    if (post_title && post_body) {
-        const response = await fetch('/api/posts', {
-            method: 'POST',
-            body: JSON.stringify({
-                post_title,
-                post_body
-            }),
-            headers: {
-                'Content-Type':'application/json'
-            }
-        });
-
-        console.log(response);
-        if (response.ok) {
-            document.location.replace('/dashboard');
-        }
+    const title = document.querySelector('#post-title').value.trim();
+    const body = document.querySelector('#post-body').value.trim();
+  
+    if (title && body) {
+      const res = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, body }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      console.log(res);
+      if (res.ok) {
+        document.location.replace('/dashboard');
+      }
     } else {
-        alert('Both fields are required.');
+      alert('Both fields are required.');
     }
-};
-
-document.querySelector('.new-post-form').addEventListener('submit', newPostFormHandler);
+  };
+  
+  document.querySelector('.new-post-form').addEventListener('submit', submitNewPost);
+  
